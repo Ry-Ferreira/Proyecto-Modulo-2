1d680baa498d14c424320e9d075d360286d3e1b3
Ryan <ryferreira1@outlook.es>
2020-08-26 22:59:22 -0300

    Updates

---
+++ precache-manifest.606765c196a93f490c9e04538ed81c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9572e7d43503017b1f1541e90ea58c7f",
+    "url": "/Proyecto-Modulo-2/index.html"
+  },
+  {
+    "revision": "be43a9d971cb532c3c8d",
+    "url": "/Proyecto-Modulo-2/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "413c33ad6454c1644c06",
+    "url": "/Proyecto-Modulo-2/static/js/2.1c9ecc3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Proyecto-Modulo-2/static/js/2.1c9ecc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be43a9d971cb532c3c8d",
+    "url": "/Proyecto-Modulo-2/static/js/main.d756f053.chunk.js"
+  },
+  {
+    "revision": "491024cd3a5d99dae542",
+    "url": "/Proyecto-Modulo-2/static/js/runtime-main.8f12714b.js"
+  }
+]);
